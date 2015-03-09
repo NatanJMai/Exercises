89b96e9347d30beafa57e053d3b55318d7faec63
natanjmai <natan.mai@hotmail.com>
2015-03-09 18:02:49 -0300

    Comments

--- calc1.hs
+++ calc1.hs
@@ -4,8 +4,8 @@
 	a list with the values between a and b.
 
 	Example: 
-		f(x) = (2 * x) - 10
-		> function 4 6 
+	f(x) = (2 * x) - 10
+    		> function 4 6 
 			result = 5.0 (OK)
 
 		> function 4 7
@@ -28,11 +28,11 @@ f x y =
 
 function :: (Num a, Fractional a, Ord a) => a -> a -> a
 function a b  
-	| fx x1                 <= (-1.0)       	= x1 
-	| fx x1                  == 0            	= x1
-	| fx a * fx x1         == 0           	= x1
-	| fx a * fx x1           > 0            	= function x1 b
-	| fx a * fx x1           < 0            	= function a x1
-	| fx b * fx x1           > 0            	= function a x1
-	| fx b * fx x1           < 0            	= function x1 b
-	where                      x1            	= (a + b) / 2
+	| fx x1			<= (-1.0)  	= x1 
+	| fx x1			== 0       	= x1
+	| fx a * fx x1	== 0       	= x1
+	| fx a * fx x1       > 0		= function x1 b
+	| fx a * fx x1       < 0		= function a x1
+	| fx b * fx x1       > 0		= function a x1
+	| fx b * fx x1       < 0		= function x1 b
+	where                  x1        	= (a + b) / 2
