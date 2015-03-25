ea7c43073c9dcae047930ce5466de81857d099e1
natanjmai <natan.mai@hotmail.com>
2015-03-25 18:20:07 -0300

    First.

--- calc4.hs
+++ calc4.hs
@@ -5,6 +5,9 @@ import Data.Maybe (fromJust)
     Gauss Method
 -}
 
+teste :: Int
+teste = 2
+
 list :: (Num a) => [[a]]
 list =
     [
@@ -13,14 +16,25 @@ list =
         [2, -3, 1, (-1)]]
 
 
-calcM :: (Fractional a) => a -> a -> a
-calcM ayx axx =
-    ayx / axx
+--calcM :: (Num a , Fractional a, Integral a) =>a -> a -> a
+calcM a b =
+    a / b
 
 -- l2 = l2 - M . l1
 
-calculoLinha :: (Integral a, Num a ) => [[a]] -> Int -> [a]
-calculoLinha c num =
-    [ x - m * y | x <- c !! num]
-    where m = 2 ;
-                y  = c !! (num - 1) !! 2
+{- f :: (Num a, Fractional a)=> a -> a -> a
+f x y = do
+    let m = calcM 4 2
+    x - m * y -}
+
+--calculo :: (Num a, Fractional a, Integral a) => [a] -> [a] -> [a]
+calculo xs ys = do
+    zipWith ( \x y -> y - m * x ) xs ys
+        where m = calcM (ys !! 0) (xs !! 0)
+
+--gauss :: (Num a, Fractional a, Integral a) => [[a]] -> [a]
+gauss xs = do
+    [[x | x <- xs !! 0]] ++ [[y |y <- calculo (xs !! 0)(xs !! 1)]] ++ [[z | z <-  calculo(xs !! 0)(xs !! 2)]]
+
+    --[ | x <- xs !! 0, y <- calculo (xs !! 0)(xs !! 1), z <-  calculo(xs !! 0)(xs !! 2)]
+    --xs !! 0 ++ calculo (xs !! 0)(xs !! 1) ++  calculo(xs !! 0)(xs !! 2)
