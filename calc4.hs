81c53dfe97808787ddf9b295d6f0c258455727e7
natanjmai <natan.mai@hotmail.com>
2015-03-30 15:36:28 -0300

    Trying

--- calc4.hs
+++ calc4.hs
@@ -5,43 +5,38 @@ import Data.Maybe (fromJust)
     Gauss Method
 -}
 
-list :: (Num a) => [[a]]
-list =
-    [
-        [2, 3, (-1), 5],
-        [4, 4, (-3), 3],
-        [2, -3, 1, (-1)]]
-
-
 --calcM :: (Num a , Fractional a, Integral a) =>a -> a -> a
 calcM a b =
     a / b
-
--- l2 = l2 - M . l1
-
-{- f :: (Num a, Fractional a)=> a -> a -> a
-f x y = do
-    let m = calcM 4 2
-    x - m * y -}
 
 --calculo :: (Num a, Fractional a, Integral a) => [a] -> [a] -> [a]
 calculo xs ys ind= do
     zipWith ( \x y -> y - m * x ) xs ys
         where m = calcM (ys !! ind) (xs !! ind)
 
-
-result xs =
+{-result xs =
     [xs !! 0] ++
-    gauss'(gauss xs 0 1) 1 2
+    gauss'(gauss xs 1 0 1) 2 1 1-}
 
---gauss :: (Num a, Fractional a, Integral a) => [[a]] -> [a]
-gauss xs piv k =
-    [[x | x <- xs !! a]] ++  [[y |y <- calculo (xs !! a)(xs !! (a + 1)) 0 ]]  ++  [[z | z <-  calculo(xs !! a)(xs !! (a + 2)) 0]]
-      where a = k - 1
+rssss xs =
+    [xs !! 0] ++
+    gauss(gauss xs 1 0 1) 2 1 1
 
 
- --[ | x <- xs !! 0, y <- calculo (xs !! 0)(xs !! 1), z <-  calculo(xs !! 0)(xs !! 2)]
- --xs !! 0 ++ calculo (xs !! 0)(xs !! 1) ++  calculo(xs !! 0)(xs !! 2)
-gauss' xs piv k =
-    [[x | x <- xs !! a]] ++  [[y |y <- calculo (xs !! a)(xs !! (a + 1)) 1 ]]
+gauss xs k tt jj
+    | k                     == 1        = first xs k ++  second xs k tt jj ++ second xs k tt (jj + 1)
+    | k                     == 2        = first xs k ++  second xs k tt jj
+
+-- segundo pivo
+gauss2 xs k tt jj =
+    first xs k ++
+    second xs k tt jj
+
+first xs k =
+    [[x | x <- xs !! a]]
         where a = k - 1
+
+second xs k tt j =
+    [[y |y <- calculo (xs !! a)(xs !! (a + j)) tt ]]
+        where a = k - 1
+
