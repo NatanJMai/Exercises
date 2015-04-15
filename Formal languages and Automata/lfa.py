3733cfe0579ce664d48227a48363a099bb390830
natanjmai <natan.mai@hotmail.com>
2015-04-15 01:14:28 -0300

    It's near, but isn't completed.

--- Formal languages and Automata/lfa.py
+++ Formal languages and Automata/lfa.py
@@ -25,16 +25,39 @@ finalS = 'q2'                   # Final states
 
 
 q0 = States('q0', (('a', 'q0'), ('b', 'q1'), ('c', 'q2'))) # q0 Definition
-q1 = States('q1', (('a', 'q1'), ('b', 'q2'), ('c', 'q2'))) # q1 Definition
-q2 = States('q2', (('a', 'q2'), ('b', 'q2'), ('c', 'q2'))) # q2 Definition
+q1 = States('q1', (('a', 'q1'), ('b', 'q0'), ('c', 'q0'))) # q1 Definition
+q2 = States('q2', (('a', 'q0'), ('b', 'q0'), ('c', 'q0'))) # q2 Definition
 
 
 statsK = (q0, q1, q2)
 
-def verifyEntry(entry):
+def verifyAllEntries(entry):
     for j in entry:
         if not j in alphbt:
             return False
+
+
+def findStateWithName(name):
+    for state in statsK:
+        if state.name == name:
+            return state
+
+    return ""
+
+def returnNextState(word, state):
+    for ind in range(0, len(state.rules)):
+        if word in state.rules[ind]:
+            return (findStateWithName(state.rules[ind][1]))
+
+
+def testAFD(entry, statsK):
+    stateNow = statsK[0]
+
+    stateNow = returnNextState('c', stateNow)
+
+    if stateNow != "":
+        print(stateNow.name)
+
 
 def main():
 
@@ -42,13 +65,14 @@ def main():
     entry = 'aaabbb'
 
     # First verification - Verify if all letters are on alphabet  #
-    if verifyEntry(entry) == False:
+    if verifyAllEntries(entry) == False:
         print("false")
         return
 
     for stt in statsK:
         print("%s => %s" % (str(stt.name), str(stt.rules)))
 
+    testAFD(entry, statsK)
 
 
 if __name__ == '__main__':
