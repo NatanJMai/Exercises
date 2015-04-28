28b9e9b34c773ec0f30d0c1d5b36525caf08bf59
natanjmai <natan.mai@hotmail.com>
2015-04-27 21:38:10 -0300

    Task implemented

--- Numeric Calculus/work01.hs
+++ Numeric Calculus/work01.hs
@@ -1,24 +1,33 @@
-n  = 5
-x  = [0, 0.1, 0.3, 0.6, 1]
-y  = [1, 2, 4.1, 8.3, 21]
+n  = 31
+x  = [1..31]
+y  = [11.7, 12.6, 12.6, 13.8, 15.3, 15  ,
+      15.3, 16.9, 16.8, 20.3, 22.2, 20.8,
+      19.1, 20.2, 21.2, 23.8, 24.8, 25.4,
+      25.9, 26.2, 26.7, 27.8, 28.5, 28.9,
+      27  , 25.8, 26.3, 25.2, 26.4, 27  , 26.2]
 
+-- Retorna o quadrado dos valores na lista X.
 xi² :: (Floating a ) => [a] -> [a]
 xi² xi =
     [x ^ 2 | x <- xi]
 
+-- Retorna o quadrado dos valores na lista Y.
 yi² :: (Floating a) => [a] -> [a]
 yi² yi =
     [y ^ 2 | y <- yi]
 
-somX    = sum(x)
-somY     = sum(y)
-somX²   = sum (xi² x)
-somY²   = sum (yi² y)
-somXY  = sum (zipWith (\x y -> x * y) x y)
+somX   = sum(x)                              -- Soma dos valores na lista X.
+somY   = sum(y)                              -- Soma dos valores na lista Y.
+somX²  = sum (xi² x)                         -- Soma dos quadrados de X.
+somY²  = sum (yi² y)                         -- Soma dos quadrados de Y.
+somXY  = sum (zipWith (\x y -> x * y) x y)   -- Multiplicação dos valores de X, Y
 
-a = (n * somXY - (somX * somY)) / ((n * somX²) - (somX ^ 2))
-b = (somY - (a * somX)) / n
-r² = ((somXY - (somX * somY) / n) ^ 2) / ( (somX² - ((somX) ^ 2) / n) * (somY² - (somY ^ 2) / n))
+-- Fórmula de a
+a   = (n * somXY - (somX * somY)) / ((n * somX²) - (somX ^ 2))
 
+-- Fórmula de b
+b   = (somY - (a * somX)) / n
 
+-- Fórmula de r²
+r²  = ((somXY - ((somX * somY) / n)) ^ 2) / ( (somX² - ((somX) ^ 2) / n) * (somY² - ((somY ^ 2) / n)))
 
