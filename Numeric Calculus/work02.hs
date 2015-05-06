a24b67421cb87f137cc972d3fa292496f179e06c
NatanJMai <natan.mai@hotmail.com>
2015-05-06 17:24:50 -0300

    New function

--- Numeric Calculus/work02.hs
+++ Numeric Calculus/work02.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 -- Ajuste Multilinear
 -- Xt * X * B = Xt * Y
 
@@ -5,11 +7,27 @@ n = 8
 a = [[-1, 0 , 1, 2, 4, 5, 5, 6],
      [-2, -1, 0, 1, 1, 2, 3, 4]]
 
-y = [13, 11, 9, 4, 11, 9, 1, -1]
+y = [[13, 11, 9, 4, 11, 9, 1, -1]]
 
 xT = [[1, 1, 1, 1, 1, 1, 1, 1]] ++ a
 
 x  = zipWith3(\x y z -> [x, y, z]) (xT !! 0) (xT !! 1) (xT !! 2)
 
 
+for l1 l2 lim =
+    [fff l1 l2 lim vl | vl <- [0..lim]]
+
+
+fff l1 l2 nn jj =
+    [sum(zipWith(\n y -> n * y) (colN yy l1) (l2 !! jj)) | yy <- [0..nn]]
+
+colN n matrix =
+    transpose matrix !! n
+
+
+xTx = for x xT 2
+xTy = [(for x y  2) !! 0]
+
+
+final = xTx ++ xTy
 
