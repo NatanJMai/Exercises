a2308b3ce5eba189cc24468b5e4ca84719543429
NatanJMai <natan.mai@hotmail.com>
2015-05-21 18:28:44 -0300

    Update

--- Numeric Calculus/EulerMethod.hs
+++ Numeric Calculus/EulerMethod.hs
@@ -2,19 +2,43 @@
 -- Computer Science
 -- Natan J. Mai
 
-b = 2
+b = 1
 a = 0
-n = 5
+n = 0.1
 
 h = (b - a) / n
 
-f :: (Floating a) => a -> a -> a
 f x y = 
-   x + y
-
-y = [a]
-
-x = [ a + (i * h) | i <- [0 .. n]]
+    x - y + 2
 
 
---yi = yi - 1 + h * f(xi - 1, yi - 1)
+y = [f a 0]
+
+x = [ a + (i * n) | i <- [0 .. h]]
+
+{--
+
+y1 = y0 + h * f(x0, y0)
+y2 = y1 + h * f(x1, y1)
+y3 = y2 + h * f(x2, y2) --}
+
+yi 0 = y !! 0
+yi z = 
+    (y !! (z - 1)) + (n * f (x !! (z - 1))(y !! (z - 1)))
+
+     
+--let bb = [ y ++ form i | i <- [1..h]]
+
+elemE a n = 
+    a !! n
+
+ff yy i 
+    | i                 > 1 	= yy
+    | otherwise			= a ++ (ff a (i + 1)) 
+    where a 			= yy ++ [(elemE yy i) * h] 
+   
+
+
+
+
+--yi = yi - 0 + h * f(xi - 1, yi - 1)
