930e7627d85f66d0647df812603a0ae86ffaae98
NatanJMai <natan.mai@hotmail.com>
2015-05-23 11:31:28 -0300

    Finally

--- Numeric Calculus/EulerMethod.hs
+++ Numeric Calculus/EulerMethod.hs
@@ -2,45 +2,34 @@
 -- Computer Science
 -- Natan J. Mai
 
-b = 1
-a = 0
-n = 0.1
+b = 3
+a = 1
+h = 0.1
+n = (b - a) / h
+c = round (n - 1):: Int
 
-h = (b - a) / n
 
 f x y =
-  x - y + 2
+  (y / x) - (y / x) ^ 2
 
+y = [1]
 
-y = [f a 0]
+x = [ a + (i * h) | i <- [0 .. n - 1]]
 
-x = [ a + (i * n) | i <- [0 .. h]]
-
-{--
-
-y1 = y0 + h * f(x0, y0)
-y2 = y1 + h * f(x1, y1)
-y3 = y2 + h * f(x2, y2) --}
 
 yi 0 = y !! 0
 yi z =
-  (y !! (z - 1)) + (n * f (x !! (z - 1))(y !! (z - 1)))
+  (y !! (z - 1)) + (h * f (x !! (z - 1))(y !! (z - 1)))
 
---let bb = [ y ++ form i | i <- [1..h]]
 
 elemE a n =
   a !! n
 
 
 ff yy i
-  | i             > 9     = yy
+  | i             > c     = yy
   | otherwise             = ff a (i + 1)
-  where a                 = yy ++ [(elemE yy i) + (n * f (elemE x i)(elemE yy i))]
+  where a                 = yy ++ [(elemE yy i) + (h * f (elemE x i)(elemE yy i))]
 
 
 
--- ++ (ff a (i + 1))
-
-
-
---yi = yi - 0 + h * f(xi - 1, yi - 1)
