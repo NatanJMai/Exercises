06f37d448ca15433bae3a7eb35e548cc1312edb6
NatanJMai <natan.mai@hotmail.com>
2015-06-03 18:19:49 -0300

    RungeK

--- Numeric Calculus/003/RungeKutta.hs
+++ Numeric Calculus/003/RungeKutta.hs
@@ -1,38 +1,28 @@
--- Runge Kutta method.
+  -- Runge Kutta method.
 b = 2
 a = 0
 n = 5
 h = 0.4
-c = round(n) :: Int
+c = round(n - 1) :: Int
 x = [a + (i * h) | i <- [0..n]]
 
 y1 = [2]
 y  = ff y1 0
 
-
-f x y = 
+f x y =
     x - y + 2
 
-elemE xs n = 
+elemE xs n =
     xs !! n
 
-k1 i yy = 
-    h * f (elemE x i)(elemE yy i)
-
-k2 i yy = 
-    h * f ((elemE x i) + (1/2) * h)((elemE yy i) + (1/2) * (k1 i yy)) 
-
-k3 i yy = 
-    h * f ((elemE x i) + (1/2) * h) ((elemE yy i) + (1/2) * (k2 i yy))
-
-k4 i yy = 
-    h * f ((elemE x i) + (1/2) * h) ((elemE yy i) + (1/2) * (k3 i yy))
-
 calcK i yy =
-    (elemE yy i) + (1/6) * (((k1 i yy) + (k2 i yy) + (k3 i yy) + (k4 i yy)))
+    (elemE yy i) + (1/6) * (a +(2 * b) + (2 * c) + d)
+    where a = h * f (elemE x i)(elemE yy i)
+          b = h * f ((elemE x i) + (h/2))((elemE yy i) + (a / 2))
+          c = h * f ((elemE x i) + (h/2))((elemE yy i) + (b / 2))
+          d = h * f ((elemE x i) + (h/2))((elemE yy i) + (c / 2))
 
-
-ff yy i 
+ff yy i
   | i           > c   = yy
   | otherwise         = ff a (i + 1)
   where a             = yy ++ [calcK i yy]
