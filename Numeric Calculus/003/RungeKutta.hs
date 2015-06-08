d47abeaebe676d915ffc82590e14e5f6a2ced42f
NatanJMai <natan.mai@hotmail.com>
2015-06-08 18:13:39 -0300

    Update RungeKutta.

--- Numeric Calculus/003/RungeKutta.hs
+++ Numeric Calculus/003/RungeKutta.hs
@@ -1,26 +1,46 @@
-  -- Runge Kutta method.
-b = 2
-a = 0
-n = 5
-h = 0.4
+b = 3
+a = 1
+n = 8 
+h = 0.25
 c = round(n - 1) :: Int
 x = [a + (i * h) | i <- [0..n]]
 
-y1 = [2]
+
+k1 = []
+k2 = []
+k3 = []
+k4 = []
+
+y1 = [1]
 y  = ff y1 0
 
-f x y =
-    x - y + 2
 
-elemE xs n =
-    xs !! n
+k1' x y i = 
+    h * f (elemE x i) (elemE y i)
+
+k2' x y i = 
+    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k1' x y i)))
+
+k3' x y i = 
+    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k2' x y i)))
+
+k4' x y i =   
+    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k3' x y i)))
+
+f x y = (y / x) - ((y / x) ^ 2)
+
+elemE xs n = xs !! n
 
 calcK i yy =
-    (elemE yy i) + (1/6) * (a +(2 * b) + (2 * c) + d)
-    where a = h * f (elemE x i)(elemE yy i)
-          b = h * f ((elemE x i) + (h/2))((elemE yy i) + (a / 2))
-          c = h * f ((elemE x i) + (h/2))((elemE yy i) + (b / 2))
-          d = h * f ((elemE x i) + (h/2))((elemE yy i) + (c / 2))
+        (elemE yy i) + (1/6) * (a +(2 * b) + (2 * c) + d)
+            where a = k1' x yy i 
+                  --h * f (elemE x i)(elemE yy i)
+                  b = k2' x yy i 
+                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (a / 2))
+                  c = k3' x yy i 
+                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (b / 2))
+                  d = k4' x yy i 
+                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (c / 2))
 
 ff yy i
   | i           > c   = yy
