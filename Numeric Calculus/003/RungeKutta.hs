135c3cba6cb085182e4c01e40e6a0d3fc064ce4c
NatanJMai <natan.mai@hotmail.com>
2015-06-09 00:56:05 -0300

    Final

--- Numeric Calculus/003/RungeKutta.hs
+++ Numeric Calculus/003/RungeKutta.hs
@@ -1,46 +1,53 @@
+-- Parametros
 b = 3
 a = 1
-n = 8 
-h = 0.25
-c = round(n - 1) :: Int
-x = [a + (i * h) | i <- [0..n]]
+n = (b - a) / h
+h = 0.05
 
-
-k1 = []
-k2 = []
-k3 = []
-k4 = []
-
+-- Definicoes
+c  = round(n - 2) :: Int
+x  = [a + (i * h) | i <- [1..n]]
 y1 = [1]
-y  = ff y1 0
+
+-- Resultados
+y'   = map (f') x
+y    = ff y1 0
+erro = zipWith (\ x y -> abs( x - y )) y y'
+
+main = do
+  let zer = map (show) x  
+  let one = map (show) y
+  let two = map (show) y'
+  let thr = map (show) erro
+  let fou = ["--------X---------"] ++ zer ++ ["\n---------Y--------"] ++ one ++ ["\n---------Y'-------"] ++ two ++  ["\n---------E--------"] ++ thr 
+  let fiv = unlines fou
+  putStrLn (fiv)
+
 
 
 k1' x y i = 
-    h * f (elemE x i) (elemE y i)
+  h * f (elemE x i) (elemE y i)
 
 k2' x y i = 
-    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k1' x y i)))
+  h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k1' x y i)))
 
 k3' x y i = 
-    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k2' x y i)))
+  h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k2' x y i)))
 
 k4' x y i =   
-    h * (f (elemE x i + (h * 0.5)) (elemE y i + (0.5 * k3' x y i)))
+  h * (f ((elemE x i) + h) ((elemE y i) + k3' x y i))    
 
-f x y = (y / x) - ((y / x) ^ 2)
+f x y  = (y / x) - ((y / x) ^ 2)
+f' x   = x / (1 + log (x)) 
 
 elemE xs n = xs !! n
 
 calcK i yy =
-        (elemE yy i) + (1/6) * (a +(2 * b) + (2 * c) + d)
-            where a = k1' x yy i 
-                  --h * f (elemE x i)(elemE yy i)
-                  b = k2' x yy i 
-                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (a / 2))
-                  c = k3' x yy i 
-                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (b / 2))
-                  d = k4' x yy i 
-                  --h * f ((elemE x i) + (h/2))((elemE yy i) + (c / 2))
+  (elemE yy i) + (1/6) * (a +(2 * b) + (2 * c) + d)
+     where a = k1' x yy i 
+           b = k2' x yy i 
+           c = k3' x yy i 
+           d = k4' x yy i 
 
 ff yy i
   | i           > c   = yy
