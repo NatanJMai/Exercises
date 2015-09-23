9b9d0eb5ef7c7b3f748fb62e6f7befaf067d2fac
NatanJMai <natan.mai@hotmail.com>
2015-09-23 01:02:41 -0300

    Problem10.hs update

--- 99Exercises/1-10/Problem10.hs
+++ 99Exercises/1-10/Problem10.hs
@@ -6,6 +6,16 @@
 
 import Data.List
 
+data ListItem a = Single a | Multiple Int a deriving (Show)
+
+p x = 
+   map(\y -> (s (length y) y)) (group x)
+--   map(\y -> (length y, y)) (group x)
+
+s 1 x = (Single x)
+s n x = Multiple n x
+
 main = do
   name <- getLine
-  print(map(\x -> (length x, x)) (group name))
+  print(p name)
+  --print(map(\x -> (length x, x)) (group name))
